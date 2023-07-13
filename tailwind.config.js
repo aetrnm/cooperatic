ab3436ddab8a1ec9001b6a60a7f28367e3f9cef9
Oleh Hnat <oleggnatkovskii@gmail.com>
2023-07-13 21:48:14 +0200

    refactor: add login htmk to tailwind styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 export default {
   content: [
     "./index.html",
+    "./login.html",
     "./src/*.{js,ts,jsx,tsx}",
     "./src/components/*.{js,ts,jsx,tsx}",
     "./src/screens/*.{js,ts,jsx,tsx}",

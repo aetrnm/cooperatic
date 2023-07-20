529bf3753046fd35ebb5d158c97d54e8111276b8
Oleh Hnat <oleggnatkovskii@gmail.com>
2023-07-20 18:04:10 +0200

    refactor: remove unused tauri plugin

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -25,7 +25,6 @@ async fn add_to_db_impl(email: &str, name: &str, created: &str, password: &str) 
 fn main() {
     let app = tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::default().build())
-        .plugin(tauri_plugin_sql::Builder::default().build())
         .invoke_handler(tauri::generate_handler![add_to_db])
         .build(tauri::generate_context!())
         .expect("error while building tauri application");

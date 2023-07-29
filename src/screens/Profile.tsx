8ddff748165a04c026f681a66f5b0c1731828ad6
Oleh Hnat <oleggnatkovskii@gmail.com>
2023-07-29 20:53:06 +0200

    refactor: markup change

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import { Store } from "tauri-plugin-store-api";
+const store = new Store(".settings.dat");
 
 interface UserProfile {
   username: string;
@@ -23,7 +25,8 @@ function Profile() {
     const fetchProfileData = async () => {
       const userProfile = await fetchUserProfile();
 
-      setUsername(userProfile.username);
+      const storedEmail: any = await store.get("loggedInEmail");
+      setUsername(storedEmail.value);
       setProfilePicture(userProfile.profilePicture);
       setJoinDate(userProfile.joinDate);
       setJoinedGroups(userProfile.joinedGroups);
@@ -41,13 +44,13 @@ function Profile() {
           return {
             name: groupName,
             image: "https://fakeimg.pl/300/",
-            email: "email@flowbite.com",
+            email: "email@cooperatic.com",
           };
         });
 
         resolve({
           username: "Oleh",
-          profilePicture: "https://fakeimg.pl/300/",
+          profilePicture: "https://fakeimg.pl/500/",
           joinDate: "July, 2023",
           joinedGroups,
         });
@@ -57,45 +60,35 @@ function Profile() {
 
   return (
     <div className="p-4">
-      <h1 className="text-2xl mb-4">Profile</h1>
-      <div className="flex items-center text-lg mb-4">
-        <p className="mr-4">Username:</p>
-        <p>{username}</p>
-      </div>
-      <div className="flex items-center text-lg mb-4">
-        <p className="mr-4">Profile Picture:</p>
-        <img
-          src={profilePicture}
-          alt="Profile"
-          className="w-12 h-12 rounded-full"
-        />
-      </div>
-      <div className="flex items-center text-lg mb-4">
-        <p className="mr-4">Join Date:</p>
-        <p>{joinDate}</p>
+      <h1 className="text-2xl mb-4 font-semibold">Profile</h1>
+      <div className="flex items-center space-x-4 mb-4">
+        <div className="w-20 h-20 rounded-full overflow-hidden">
+          <img src={profilePicture} alt="Profile" />
+        </div>
+        <div>
+          <p className="text-lg font-medium">{username}</p>
+          <p className="text-gray-500">Member since: {joinDate}</p>
+        </div>
       </div>
       <div>
-        <p className="text-lg">Joined Groups:</p>
-        <ul className="max-w-md divide-y divide-gray-200 dark:divide-gray-700">
-          {joinedGroups.map((group) => (
-            <li key={group.name} className="py-3 sm:py-4">
-              <div className="flex items-center space-x-4">
-                <div className="flex-shrink-0">
-                  <img
-                    className="w-8 h-8 rounded-full"
-                    src={group.image}
-                    alt={group.name}
-                  />
+        <p className="text-lg font-medium mb-2">Joined Groups:</p>
+        <ul className="space-y-2">
+          {joinedGroups.map((group, index) => (
+            <li key={group.name}>
+              <div className="flex items-center space-x-2">
+                <div className="w-8 h-8 rounded-full overflow-hidden">
+                  <img src={group.image} alt={group.name} />
                 </div>
-                <div className="flex-1 min-w-0">
-                  <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
+                <div>
+                  <p className="text-base font-medium text-gray-900">
                     {group.name}
                   </p>
-                  <p className="text-sm text-gray-500 truncate dark:text-gray-400">
-                    {group.email}
-                  </p>
+                  <p className="text-sm text-gray-500">{group.email}</p>
                 </div>
               </div>
+              {index !== joinedGroups.length - 1 && (
+                <hr className="my-2 border-gray-200 dark:border-gray-700" />
+              )}
             </li>
           ))}
         </ul>
